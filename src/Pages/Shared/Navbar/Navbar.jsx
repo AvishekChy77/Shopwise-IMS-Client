4c40a139f3573350079ee86b890aaddd54dfe409
AvishekChy77 <avishekchowdhury71@gmail.com>
2024-07-07 14:25:45 +0600

    update navbar

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -99,7 +99,7 @@ const Navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 text-black rounded-box w-52"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-white text-black rounded-box w-52"
             >
               <li>
                 <button className="btn btn-sm btn-ghost">
