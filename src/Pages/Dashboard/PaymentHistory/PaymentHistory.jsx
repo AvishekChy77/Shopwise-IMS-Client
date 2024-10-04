74c945c8d39f0e2e617be5de122069142815a7bb
AvishekChy77 <avishekchowdhury71@gmail.com>
2024-10-05 02:19:20 +0600

    added file upload option in register page

--- src/Pages/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/Pages/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import ReactApexChart from "react-apexcharts";
 import { Helmet } from "react-helmet-async";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { AuthContext } from "../../../Provider/AuthProvider";
@@ -7,15 +8,69 @@ import { AuthContext } from "../../../Provider/AuthProvider";
 const PaymentHistory = () => {
   const { user } = useContext(AuthContext);
   const axiosSecure = useAxiosSecure();
+  const [saleschartData, setSalesChartData] = useState({
+    series: [
+      {
+        name: "Sales",
+        data: [],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          enabled: false,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "straight",
+      },
+      title: {
+        text: "Sales count by Month",
+        align: "left",
+      },
+      grid: {
+        row: {
+          colors: ["transparent", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+        ],
+      },
+    },
+  });
 
   const { data: payments = [] } = useQuery({
     queryKey: ["payments", user.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/payments/${user.email}`);
+      const newSalesChartData = {
+        ...saleschartData,
+        series: [
+          { name: "Sales", data: [10, 41, 35, 51, 49, 62, 69, 91, 148] },
+        ],
+      };
+      setSalesChartData(newSalesChartData);
       return res.data;
     },
   });
   console.log(payments);
+  console.log(saleschartData);
   const totalInvest = payments?.reduce(
     (total, item) => total + item.productlists.productionCost,
     0
@@ -31,13 +86,21 @@ const PaymentHistory = () => {
         <title>ShopWise | Sales Summary</title>
       </Helmet>
       <div>
-        <div className=" p-4 my-5 max-w-3xl mx-auto bg-slate-100 pb-10 lg:px-10">
+        <div className=" p-4 my-5 max-w-3xl mx-auto  pb-10 lg:px-10">
           <div className="grid mb-5 grid-cols-1 md:grid-cols-3 gap-5 align-middle">
-            <div className="h-32 lg:h-40 rounded text-white text-lg font-medium flex items-center justify-center shadow-lg  bg-gradient-to-r from-[#ff3434] to-[#e09c9c]">
+            {/* <div className="h-32 lg:h-40 rounded text-white text-lg font-medium flex items-center justify-center shadow-lg  bg-gradient-to-r from-[#ff3434] to-[#e09c9c]">
               <div className="flex items-start justify-center flex-col-reverse pl-2">
                 <p>Total Sale</p>
                 <p>{payments?.length}</p>
               </div>
+            </div> */}
+            <div className="h-32 lg:h-40 rounded">
+              <ReactApexChart
+                options={saleschartData.options}
+                series={saleschartData.series}
+                type="line"
+                height={200}
+              />
             </div>
             <div className="h-32 lg:h-40 rounded text-white text-lg font-medium flex items-center justify-center shadow-lg  bg-gradient-to-r from-green-500 to-green-200">
               <div className="flex items-start justify-center flex-col-reverse pl-2">
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { updateProfile } from "firebase/auth";
 import Lottie from "lottie-react";
 import { useContext } from "react";
@@ -12,6 +13,8 @@ import animationData from "../../assets/login.json";
 import SocialLogin from "../Login/SocialLogin";
 
 const Register = () => {
+  const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+  const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
   const {
     register,
     handleSubmit,
@@ -22,41 +25,52 @@ const Register = () => {
   const goTo = useNavigate();
   const axiosPublic = useAxiosPublic();
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     console.log(data);
-    createUser(data.email, data.password)
-      .then((res) => {
-        const loggedUser = res.user;
-        console.log(loggedUser);
+    //   upload img to imgBB and get url
+    const selectedimage = data.photoURL[0];
+    const dataForm = new FormData();
+    dataForm.append("image", selectedimage);
+    console.log(selectedimage, dataForm);
+    const res = await axios.post(image_hosting_api, dataForm);
+    console.log(res.data);
+    if (res.data.success) {
+      createUser(data.email, data.password)
+        .then((response) => {
+          const loggedUser = response.user;
+          console.log(loggedUser);
 
-        updateProfile(auth.currentUser, {
-          displayName: data.displayName,
-          photoURL: data.photoURL,
-        })
-          .then((res) => {
-            // Profile information updated successfully
-            // add to db
-            const userInfo = {
-              name: data.displayName,
-              email: data.email,
-            };
-            axiosPublic.post("/users", userInfo).then((res) => {
-              if (res.data.insertedId) {
-                toast("Account created succesfully!");
-                reset();
-                goTo("/createStore");
-              }
-            });
-            console.log(res);
+          updateProfile(auth.currentUser, {
+            displayName: data.displayName,
+            photoURL: res.data.data.display_url,
           })
-          .catch((error) => {
-            // Handle profile update errors
-            console.log(error.message);
-          });
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
+            .then((res) => {
+              // Profile information updated successfully
+              // add to db
+              const userInfo = {
+                name: data.displayName,
+                email: data.email,
+              };
+              axiosPublic.post("/users", userInfo).then((res) => {
+                if (res.data.insertedId) {
+                  toast("Account created succesfully!");
+                  reset();
+                  goTo("/createStore");
+                }
+              });
+              console.log(res);
+            })
+            .catch((error) => {
+              // Handle profile update errors
+              console.log(error.message);
+            });
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    } else {
+      toast.error("Choose a valid file!");
+    }
   };
 
   return (
@@ -88,13 +102,19 @@ const Register = () => {
                 <label className="label">
                   <span className="label-text text-black">Photo</span>
                 </label>
-                <input
-                  type="text"
+                {/* <input
+                  type="file"
                   {...register("photoURL")}
                   placeholder="Your photo url"
                   name="photoURL"
                   className="input bg-white border border-gray-300 shadow-sm focus:outline-none focus:ring-1 focus:ring-slate-400"
                   required
+                /> */}
+                <input
+                  type="file"
+                  {...register("photoURL")}
+                  name="photoURL"
+                  className="file-input file-input-bordered file-input-md w-full"
                 />
               </div>
               <div className="form-control">
@@ -127,10 +147,14 @@ const Register = () => {
                   className="input bg-white border border-gray-300 shadow-sm focus:outline-none focus:ring-1 focus:ring-slate-400"
                 />
                 {errors.password?.type === "required" && (
-                  <span className="text-red-500 text-sm mt-1">Don't forget to choose your Password</span>
+                  <span className="text-red-500 text-sm mt-1">
+                    Don't forget to choose your Password
+                  </span>
                 )}
                 {errors.password?.type === "minLength" && (
-                  <span className="text-red-500 text-sm mt-1">Password must be atleast 6 characters</span>
+                  <span className="text-red-500 text-sm mt-1">
+                    Password must be atleast 6 characters
+                  </span>
                 )}
                 {errors.password?.type === "pattern" && (
                   <span className="text-red-500 text-sm mt-1">
